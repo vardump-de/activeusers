9221d2b914d3fd295e9b1fa01d1dfd7f417ccfc2
rmdev <roman@vardump.de>
2024-12-10 13:04:16 +0100

    Add italian translation

---
+++ src/translations/it/activeusers.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * activeusers plugin for Craft CMS 5.x
+ *
+ * A widget showing active Users
+ *
+ * @link      https://vardump.de
+ * @copyright Copyright (c) 2019-2024 vardump.de
+ */
+
+/**
+ * @author    vardump.de
+ * @package   Activeusers
+ * @since     3.0.2
+ */
+return [
+    'activeusers plugin loaded' => 'activeusers plugin loaded',
+    'ActiveusersWidget' => 'Utenti in linea',
+    'Message' => 'Messaggio',
+    'Message if nobody is logged in' => 'Messaggio se nessuno ha effettuato l\'accesso',
+    'Inactive Time' => 'Tempo di inattività',
+    'Minutes after the user is assumed inactive' => 'Minuti dopo che l\'utente viene considerato inattivo',
+    'minutes' => 'minuti',
+    'User limit' => 'limite utenti',
+    'Max. shown user' => 'Numero massimo di utenti visualizzati',
+    'Max. content height in px' => 'Massima altezza del contenuto in px',
+    'A scrollbar is added if the content height exceeds the height set (0 = unlimited)' => 'Viene aggiunta una barra di scorrimento se l\'altezza del contenuto supera l\'altezza impostata (0 = illimitato)',
+    'User link' => 'Collegamento utente',
+    'Link for the users. You can use the {{ user }} object' => 'Link per gli utenti. Puoi utilizzare l\'oggetto {{ user }}',
+    'Link target' => 'Obiettivo del collegamento',
+    'ex. _blank for new window' => 'es. per una nuova finestra',
+    'Show user groups' => 'Mostra gruppi di utenti'
+];
